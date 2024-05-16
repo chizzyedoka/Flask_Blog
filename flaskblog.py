20e6cf97e2112ea970b513a6137239af86fa4e6d
CHISOM EDOKA <edokachisom48@gmail.com>
2024-05-16 11:41:59 +0100

    added templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Chisom Edoka',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 25, 2024'
+    },
+     {
+        'author': 'Samuel Edoka',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 26, 2024'
+    },
+]
+
 @app.route("/")
+def greet():
+    return "hello world"
+
 @app.route("/home")
 def home():
-    return "<h1>Home Page!<h1>"
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about_page():
-    return "<h1>I'm a software Engineer</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
