c63c1e287290c674f8b1c483d02246118bcf6933
CHISOM EDOKA <edokachisom48@gmail.com>
2024-05-16 16:24:37 +0100

    forms and validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ddf638ac4d79c754ba2e59154b2a2fcb'
 
 posts = [
     {
@@ -29,6 +32,18 @@ def home():
 def about_page():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
